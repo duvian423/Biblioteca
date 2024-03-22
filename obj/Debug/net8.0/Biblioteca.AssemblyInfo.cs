4ec89164c041e250748cf129d9b40cc39fac4473
DanielMontiel <danielmontiel1600@gmail.com>
2024-03-22 11:13:37 -0500

    agregue el contenido de landing y cree un documento de estilos para Landing

--- obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
+++ obj/Debug/net8.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b2c9bed65c860c77787ac2d50831c988f50add1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58043ebe3e2b6292c3cfa067247c8b5d2115f53b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
